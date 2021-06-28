bff92aa5876e4f66af4fab3bcc7d313a64a367ba
“liadbelade” <liadb13@gmail.com>
2021-06-28 13:50:25 +0300

    taking care with conflicts

--- evenOddGame.js
+++ evenOddGame.js
@@ -16,42 +16,7 @@ const path = "./evenOddGameData.txt"
 if (!fs.existsSync(path)) {
   fs.writeFileSync("./evenOddGameData.txt", "GAME RESULTS: \n")
 }
-<<<<<<< HEAD
-
-function get2RandomDifferentNumbers(min, max) {
-  const num1 = randomInteger(min, max)
-  let num2 = randomInteger(min, max)
-  while (num1 === num2) {
-    num2 = randomInteger(min, max)
-  }
-  return { num1, num2 }
-}
-
-function checkIfNegativeEven(num) {
-  return num < 0 && num % 2 === 0
-}
-
-function createNewUserObjectIfNeeded(users, num) {
-  if (!users[num]) {
-    users[num] = { name: args[num], score: 0 }
-  }
-}
-
-function printCurrentRoundWinner(round, randNum, user) {
-  console.log(
-    `Round #${round}, random number is ${randNum}, ${user.name} score`
-  )
-}
-
-function checkWinner(winScore, user1, user2) {
-  if (user1.score === winScore) return user1
-  else if (user2.score === winScore) return user2
-}
-
-const args = process.argv.slice(2)
-=======
 fs.readFileSync(path, { encoding: "utf-8" })
->>>>>>> fs-module
 
 if (args.length < 2 || args.length > 7) {
   throw new Error("2 - 7 num players is allowed")
@@ -129,48 +94,6 @@ while (!whoWinBossFight) {
   round++
 }
 
-<<<<<<< HEAD
-console.log(
-  `${tournamentWinner.name} Wins in tourmomant!, moving to boss fight !`
-)
-
-tournamentWinner.score = 0
-const boss = { name: "boss", score: 0 }
-round = 1
-winScore = 3
-let whoWinBossFight = undefined
-
-while (!whoWinBossFight) {
-  const randNum = randomInteger(13, -5)
-
-  if (checkIfNegativeEven(randNum)) {
-    console.log(`The number is ${randNum} - Boss special ability activated !`)
-    continue
-  }
-
-  if (randNum % 2 === 0) {
-    tournamentWinner.score++
-    printCurrentRoundWinner(round, randNum, tournamentWinner)
-  } else {
-    boss.score++
-    printCurrentRoundWinner(round, randNum, boss)
-  }
-
-  const winner = checkWinner(winScore, tournamentWinner, boss)
-  if (winner) {
-    whoWinBossFight = winner
-  }
-
-  console.log(
-    `Status ${tournamentWinner.name}: ${tournamentWinner.score}, ${boss.name}: ${boss.score}`
-  )
-
-  round++
-}
-
-console.log(`${whoWinBossFight.name} Wins in boss fight!`)
-=======
 console.log(`${whoWinBossFight.name} Wins in boss fight!`)
 
 appendDataToFile(path, whoWinBossFight)
->>>>>>> fs-module
